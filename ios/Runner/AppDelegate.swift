d874fdb40949693c1b9f349af362d6ed71775e3a
Erick Llerenas <dev.llerenas@gmail.com>
2023-12-20 16:46:58 -0600

    Removido Config.swift del seguimiento de Git

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,8 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let apiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"] ?? ""
-    GMSServices.provideAPIKey(apiKey)
+    GMSServices.provideAPIKey(Config.googleMapsApiKey)
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
